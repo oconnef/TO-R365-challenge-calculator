31c88e6f54b99026714bcaf5109600e2043bc6f7
Tom O'Connell <TomO'Connell@situsamc.com>
2022-06-10 19:11:01 -0700

    Added MaxValue (1000) as per feature 5

--- TO-R365-challenge-calculator/TO-R365-CC-Tests/UnitTest1.cs
+++ TO-R365-challenge-calculator/TO-R365-CC-Tests/UnitTest1.cs
@@ -10,24 +10,8 @@ namespace TO_R365_CC_Tests
         {
             _calc = new TOCalculator.Calculator();
             _calc.Delimiters = _delimiters;
-            
-            //calc.MaxArgs = 2;
+            _calc.MaxVal = 1000;
         }
-
-        //[Test]
-        //public void TestMaximumArgumentsIsSuccess()
-        //{
-        //    string input = "20,20";
-
-        //    Assert.DoesNotThrow(() => calc.ParseInput(input));
-        //}
-
-        //[Test]
-        //public void TestMaximumArgumentsIsFailure()
-        //{
-        //    string input = "20,20,20";
-        //    Assert.Throws<ArgumentException>(() => calc.ParseInput(input));
-        //}
 
         [Test]
         public void TestNewlineDelimeterIsSuccess()
@@ -69,10 +53,6 @@ namespace TO_R365_CC_Tests
             _calc.ParseInput(input);
             Assert.That(_calc.Add(), Is.EqualTo("20"));
 
-            input = "1,5000";
-            _calc.ParseInput(input);
-            Assert.That(_calc.Add(), Is.EqualTo("5001"));
-
             input = "4,-3";
             _calc.ParseInput(input);
             Assert.That(_calc.Add(), Is.EqualTo("1"));
@@ -81,6 +61,19 @@ namespace TO_R365_CC_Tests
             _calc.ParseInput(input);
             Assert.That(_calc.Add(), Is.EqualTo("78"));
 
+        }
+
+        [Test]
+        public void TestSumsWithMaxValIsSuccess()
+        {
+            string input = "1,5000";
+            _calc.ParseInput(input);
+            Assert.That(_calc.Add(), Is.EqualTo("1"));
+
+            input = "2,1001,6";
+            _calc.ParseInput(input);
+            Assert.That(_calc.Add(), Is.EqualTo("8"));
+            
         }
     }
 }
--- TO-R365-challenge-calculator/TOR365Calculator/IR365Calculator.cs
+++ TO-R365-challenge-calculator/TOR365Calculator/IR365Calculator.cs
@@ -2,11 +2,6 @@
 {
     public interface IR365Calculator
     {
-        //removed per feature 2
-        //int MaxArgs {
-        //    get;
-        //}
-
         string[] Delimiters {
             get;
             set;

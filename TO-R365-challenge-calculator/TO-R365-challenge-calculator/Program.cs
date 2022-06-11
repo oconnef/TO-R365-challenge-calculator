af29c8d054372ba520961c91336744d8799c3acc
Tom O'Connell <TomO'Connell@situsamc.com>
2022-06-10 19:40:27 -0700

    Add input for custom delimiter per Feature 6

--- TO-R365-challenge-calculator/TO-R365-challenge-calculator/Program.cs
+++ TO-R365-challenge-calculator/TO-R365-challenge-calculator/Program.cs
@@ -7,22 +7,26 @@ namespace TO_R365_challenge_calculator
     {
         private const int maxVal = 1000;
         private static string[] delimiters = new string[] { "\n", "," };
+        private static char delimiterTrimChar = '/';
 
         static void Main(string[] args)
         {
             Calculator c = new Calculator();
             c.Delimiters = delimiters;
+            c.DelimiterTrimChar = delimiterTrimChar;
             c.MaxVal = maxVal;
-            
+
             Console.WriteLine("R365 Calculator Challenge");
-
+            Console.WriteLine();
             Console.WriteLine("Enter the data to be calculated in the format \"x,x,...\": ");
+            Console.WriteLine("*NOTE* maximum value is: " + maxVal.ToString());
 
+            string customDelimiter = Console.ReadLine();
             string inputData = Console.ReadLine();
 
             try
             {
-                c.ParseInput(inputData);
+                c.ParseInput(customDelimiter, inputData);
                 
                 Console.WriteLine("The result is:" + c.Add());
             }
